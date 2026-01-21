6f7d890caf6b75b5aff34c5b6db5685ed7165c9b
gyu98-mun <ansrb537@gmail.com>
2026-01-21 11:28:20 +0900

    2026.01.21 실습예제

--- JS/2026.01.19 ~ 20/aibe5myReact/myapp/src/ex03/Say.js
+++ JS/2026.01.19 ~ 20/aibe5myReact/myapp/src/ex03/Say.js
@@ -3,16 +3,19 @@ import React, { useState } from 'react';
 const Say = () => {
     const [message, setMessage] = useState('');
     const onclickEnter = () => setMessage('안녕하세요!');
-    const onclickLeave = () => setMessage('안녕히 가세요!');
+    const onclickLeave = () => setMessage('안녕히가세요!');
     const [color, setColor] = useState('black');
+    const onclickReset = () => {setMessage(''); setColor('black');};
     return (
         <div>
             <button onClick={onclickEnter}>입장</button>
             <button onClick={onclickLeave}>퇴장</button>
+            <button onClick={onclickReset}>초기화</button>            
             <h1 style={{color}}>{message}</h1>
-            <button onClick={() => setColor('red')}>빨간색</button>
-            <button onClick={() => setColor('green')}>초록색</button>
-            <button onClick={() => setColor('blue')}>파란색</button>
+            <button style={{color : "red"}} onClick={() => setColor('red')}>빨간색</button>
+            <button style={{color : "green"}} onClick={() => setColor('green')}>초록색</button>
+            <button style={{color : "blue"}} onClick={() => setColor('blue')}>파란색</button>
+            <button style={{color : "black"}} onClick={() => setColor('black')}>검은색</button>
         </div>
     );
 };
